9058cba0ced508a72435fc78fded5467af614487
akmaurya02 <50515618+akmaurya02@users.noreply.github.com>
2021-09-12 12:57:59 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,28 @@
+import { randomGridPosition } from "./grid.js"
+import { onSnake, expandSnake } from "./snake.js"
+
+let food = getRandomFoodPosition()
+const expansionRate = 2
+
+export function update () {
+    if (onSnake(food)) {
+        expandSnake(expansionRate)
+        food = getRandomFoodPosition()
+    }
+}
+
+export function draw (gameBoard) {
+    const foodElement = document.createElement("div")
+    foodElement.style.gridRowStart = food.y
+    foodElement.style.gridColumnStart = food.x
+    foodElement.classList.add("food")
+    gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition(){
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
+}
